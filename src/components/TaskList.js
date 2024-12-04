321753abe2170fcb799263ac096040067e066a66
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2024-12-04 21:49:24 +0530

    first commit4

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const TaskList = () => {
+function TaskList() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState('');
 
   useEffect(() => {
     const fetchTasks = async () => {
-      const res = await axios.get('https://api.bitmasters52.online/tasks');
+      const res = await axios.get('/api/tasks');
       setTasks(res.data);
     };
     fetchTasks();
   }, []);
 
   const addTask = async () => {
-    const res = await axios.post('https://api.bitmasters52.online/tasks', { title });
+    const res = await axios.post('/api/tasks', { title });
     setTasks([...tasks, res.data]);
     setTitle('');
   };
 
   const deleteTask = async (id) => {
-    await axios.delete(`https://api.bitmasters52.online/tasks/${id}`);
-    setTasks(tasks.filter((task) => task._id !== id));
+    await axios.delete(`/api/tasks/${id}`);
+    setTasks(tasks.filter(task => task._id !== id));
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+    <div className="TaskList">
+      <h1>Task List</h1>
+      <input 
+        type="text" 
+        value={title} 
+        onChange={(e) => setTitle(e.target.value)} 
       />
       <button onClick={addTask}>Add Task</button>
       <ul>
@@ -42,6 +43,6 @@ const TaskList = () => {
       </ul>
     </div>
   );
-};
+}
 
 export default TaskList;
